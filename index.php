82f8da0445b8286126a03ade1319e6c1f9d7ddc8
Claire <cmaillet2705@gmail.com>
2019-04-05 21:29:26 +0200

    Fix direct use of $_GET/$_POST Superglobal detected.

--- index.php
+++ index.php
@@ -7,105 +7,81 @@ require_once ('vendor/autoload.php');
 require_once ('controller/frontend/frontend.php');
 require_once ('config.php');
 
+$page = filter_input(INPUT_GET, 'page');
 
 //BLOG
- if (isset($_GET['page']) && $_GET['page'] == "blog") {
-     if (isset($_GET['idPage'])){
-         $page = ($_GET['idPage']);
-     }
-     else {
-         $page = 1;
-     }
+ if ($page === "blog") {
+     $idPage = filter_input(INPUT_GET, 'idPage') ?? 1;
+
      $frontend = new Frontend();
-     $render = $frontend->blog($page);
+     $render = $frontend->blog($idPage);
 
 
 //HOME
- }elseif (isset($_GET['page']) && $_GET['page'] == "home") {
+ }elseif ($page === "home") {
      $frontend = new Frontend();
      $render = $frontend->index();
 
 
 //PAGE ARTICLE
- }elseif (isset($_GET['page']) && $_GET['page'] == "article") {
-     if (isset($_GET['idPage'])){
-         $page = ($_GET['idPage']);
-     }
-     else {
-         $page = 1;
-     }
-     $name="comment";
+ }elseif ($page === "article") {
+     $idPage = filter_input(INPUT_GET, 'idPage') ?? 1;
 
+     $enteredName = filter_input(INPUT_GET, 'name');
+     $name = "comment";
      //FORMULAIRE - MODIFIER UN COMMENTAIRE SUR PAGE ARTICLE
-     if (isset($_GET['name']) && $_GET['name'] == "form_comment_update_blog") {
-         if (isset($_GET['idPage'])){
-             $page = ($_GET['idPage']);
-         }
-         else {
-             $page = 1;
-         }
-         $name="form_comment_update_blog";
+     if ($enteredName === "form_comment_update_blog") {
+         $name = "form_comment_update_blog";
      }
+
      $frontend = new Frontend();
-     $render = $frontend->article("", $name, $_GET['id_comment'] ?? -1, $_GET['id_post'], $page);
+     $render = $frontend->article("", $name, filter_input(INPUT_GET, 'id_comment') ?? -1, filter_input(INPUT_GET, 'id_post'), $idPage);
 
 
 //LOGIN & REGISTER
- }elseif (isset($_GET['page']) && $_GET['page'] == "register") {
+ }elseif ($page === "register") {
      $frontend = new Frontend();
      $render = $frontend->register();
 
- }elseif (isset($_GET['page']) && $_GET['page'] == "login") {
+ }elseif ($page === "login") {
      $frontend = new Frontend();
      $render = $frontend->login();
 
- }elseif (isset($_GET['page']) && $_GET['page'] == "add_user") {
+ }elseif ($page === "add_user") {
      $frontend = new Frontend();
-     $render = $frontend->addUser(htmlspecialchars($_POST['pseudo']), htmlspecialchars($_POST['email']), htmlspecialchars($_POST['password']), htmlspecialchars($_POST['confirm-password']));
+     $render = $frontend->addUser(htmlspecialchars(filter_input(INPUT_POST, 'pseudo')), htmlspecialchars(filter_input(INPUT_POST, 'email')), htmlspecialchars(filter_input(INPUT_POST, 'password')), htmlspecialchars(filter_input(INPUT_POST, 'confirm-password')));
 
- }elseif (isset($_GET['page']) && $_GET['page'] == "connect_user") {
+ }elseif ($page === "connect_user") {
      $frontend = new Frontend();
-     $render = $frontend->connectUser(htmlspecialchars($_POST['pseudo']), htmlspecialchars($_POST['password']));
+     $render = $frontend->connectUser(htmlspecialchars(filter_input(INPUT_POST, 'pseudo')), htmlspecialchars(filter_input(INPUT_POST, 'password')));
 
 
 //DASHBOARD - AJOUTER UN ARTICLE
- }elseif (isset($_GET['page']) && $_GET['page'] == "add_post") {
-     if (isset($_GET['idPage'])){
-         $page = ($_GET['idPage']);
-     }
-     else {
-         $page = 1;
-     }
+ }elseif ($page === "add_post") {
+     $idPage = filter_input(INPUT_GET, 'idPage') ?? 1;
+
      $frontend = new Frontend();
-     $render = $frontend->addPost(htmlspecialchars($_POST['titre']), htmlspecialchars($_POST['introduction']), htmlspecialchars($_POST['contenu']), $page);
+     $render = $frontend->addPost(htmlspecialchars(filter_input(INPUT_POST,'titre')), htmlspecialchars(filter_input(INPUT_POST,'introduction')), htmlspecialchars(filter_input(INPUT_POST,'contenu')), $idPage);
 
 
 //DASHBOARD - SUPPRIMER UN ARTICLE
- }elseif (isset($_GET['page']) && $_GET['page'] == "delete_post") {
-     if (isset($_GET['idPage'])){
-         $page = ($_GET['idPage']);
-     }
-     else {
-         $page = 1;
-     }
+ }elseif ($page === "delete_post") {
+     $idPage = filter_input(INPUT_GET, 'idPage') ?? 1;
+
      $frontend = new Frontend();
-     $render = $frontend->deletePost($_GET['id_post'], $page);
+     $render = $frontend->deletePost(filter_input(INPUT_GET,'id_post'), $idPage);
 
 
 //DASHBOARD - MODIFIER UN ARTICLE
- }elseif (isset($_GET['page']) && $_GET['page'] == "update_post") {
-     if (isset($_GET['idPage'])){
-         $page = ($_GET['idPage']);
-     }
-     else {
-         $page = 1;
-     }
+ }elseif ($page === "update_post") {
+     $idPage = filter_input(INPUT_GET, 'idPage') ?? 1;
+
      $frontend = new Frontend();
-     $render = $frontend->updatePost(htmlspecialchars($_POST['titre']), htmlspecialchars($_POST['introduction']), htmlspecialchars($_POST['contenu']), $_GET['id_post'], $page);
+     $render = $frontend->updatePost(htmlspecialchars($_POST['titre']), htmlspecialchars($_POST['introduction']), htmlspecialchars($_POST['contenu']), filter_input(INPUT_GET,'id_post'), $idPage);
 
 
 //MODIFIER UN COMMENTAIRE
- }elseif (isset($_GET['page']) && $_GET['page'] == "update_comment") {
+ }elseif ($page === "update_comment") {
      $name="";
      //BLOG
      if (isset($_GET['name']) && $_GET['name'] == "update_comment_blog") {
@@ -132,7 +108,7 @@ require_once ('config.php');
 
 
 //DASHBAORD - SUPPRIMER UN COMMENTAIRE
- }elseif (isset($_GET['page']) && $_GET['page'] == "delete_comment") {
+ }elseif ($page === "delete_comment") {
      if (isset($_GET['idPage'])){
          $page = ($_GET['idPage']);
      }
@@ -144,7 +120,7 @@ require_once ('config.php');
 
 
 //DASHBAORD - AJOUTER UN COMMENTAIRE
- }elseif (isset($_GET['page']) && $_GET['page'] == "add_comment") {
+ }elseif ($page === "add_comment") {
      if (isset($_GET['idPage'])){
          $page = ($_GET['idPage']);
      }
@@ -157,7 +133,7 @@ require_once ('config.php');
 
 
 //DASHBOARD - VALIDER UN COMMENTAIRE
- }elseif (isset($_GET['page']) && $_GET['page'] == "validation_comment") {
+ }elseif ($page === "validation_comment") {
      if (isset($_GET['idPage'])){
          $page = ($_GET['idPage']);
      }
@@ -169,7 +145,7 @@ require_once ('config.php');
 
 
 //DASHBOARD - VALIDER UN UTILISATEUR
- }elseif (isset($_GET['page']) && $_GET['page'] == "validation_user") {
+ }elseif ($page === "validation_user") {
      if (isset($_GET['idPage'])){
          $page = ($_GET['idPage']);
      }
@@ -181,7 +157,7 @@ require_once ('config.php');
 
 
 //DASHBOARD - SUPPRIMER UN UTILISATEUR
- }elseif (isset($_GET['page']) && $_GET['page'] == "delete_user") {
+ }elseif ($page === "delete_user") {
      if (isset($_GET['idPage'])){
          $page = ($_GET['idPage']);
      }
@@ -193,13 +169,13 @@ require_once ('config.php');
 
 
 //SESSION
- }elseif (isset($_GET['page']) && $_GET['page'] == "sign_out") {
+ }elseif ($page === "sign_out") {
      $frontend = new Frontend();
      $frontend->signOut();
 
 
 //AFFICHAGE DASHBOARD
- }elseif (isset($_GET['page']) && $_GET['page'] == "dashboard") {
+ }elseif ($page === "dashboard") {
      if (isset($_GET['idPage'])){
          $page = ($_GET['idPage']);
      }
